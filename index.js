42a799c529799fcecf5ee786a512439160e35117
Arun1102 <78301915+Arun1102@users.noreply.github.com>
2022-01-22 00:12:27 +0800

    changes saved

--- index.js
+++ index.js
@@ -7,16 +7,18 @@ const authRoute = require("./routes/auth");
 const productRoute = require("./routes/product");
 const cartRoute = require("./routes/cart");
 const orderRoute = require("./routes/order");
-
+const stripeRoute = require("./routes/stripe");
+const cors = require("cors");
 
 dotenv.config();
+app.use(cors())
 app.use(express.json())
 app.use("/api/user", UserTest);
 app.use("/api/auth", authRoute);
 app.use("/api/products", productRoute);
 app.use("/api/carts", cartRoute);
 app.use("/api/orders", orderRoute);
-
+app.use("/api/checkout", stripeRoute);
 
 mongoose.connect(process.env.MONGO_URL)
 .then(()=>{console.log("Database connected succesfully")})
